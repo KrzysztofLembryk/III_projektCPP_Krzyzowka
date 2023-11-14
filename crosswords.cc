333823859e6dcfc0d06de462edeb874a3309854c
Kamil Dybek <kam.dyb1@gmail.com>
2023-11-14 16:55:11 +0100

    merge

--- crosswords.cc
+++ crosswords.cc
@@ -1,42 +1,355 @@
+#include <typeinfo>
+#include <stdexcept>
 #include "crosswords.h"
 
 #define LETTER_EXISTS(y, x) m_letters.count(pos_t(y, x)) != 0
 #define LETTER(y, x) m_letters.at(pos_t(y, x))
 
-Crossword::Crossword(Crossword&& other) noexcept
-    : m_words(std::move(other.m_words)),
-      m_rectArea(std::exchange(other.m_rectArea, DEFAULT_EMPTY_RECT_AREA)),
-      m_letters(std::move(other.m_letters))
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::move;
+using std::pair;
+using std::string;
+using std::min;
+using std::max;
+
+using pos_t = pair<size_t, size_t>;
+using dim_t = pair<size_t, size_t>;
+
+// -----WORD CLASS-----
+
+// Constructors:
+Word::Word(size_t x, size_t y, orientation_t orient, std::string const& word)
+        : m_posAndOrient(x, y, orient),
+          m_word(!word.empty() ? word : DEFAULT_WORD)
 {
 }
 
-Crossword &Crossword::operator=(const Crossword &other)
+Word::Word(const Word& other) = default;
+
+Word::Word(Word&& other) noexcept = default;
+
+// Destructors:
+Word::~Word() = default;
+
+// Operators:
+Word& Word::operator=(const Word& rhs) = default;
+
+Word& Word::operator=(Word&& rhs) noexcept = default;
+
+bool Word::operator==(const Word& other) const
 {
-    m_words = other.m_words;
-    m_rectArea = other.m_rectArea;
-    m_letters = other.m_letters;
+    return m_posAndOrient == other.m_posAndOrient;
+}
+
+auto Word::operator<=>(const Word& other) const
+{
+    return m_posAndOrient <=> other.m_posAndOrient;
+}
+
+// Getters:
+pos_t Word::get_start_position() const
+{
+    return m_posAndOrient.getPos();
+}
+
+pos_t Word::get_end_position() const
+{
+    pos_t p = m_posAndOrient.getPos();
+
+    if (m_posAndOrient.getOrient() == H)
+    {
+        return {p.first + m_word.size(), p.second};
+    } else
+    {
+        return {p.first, p.second + m_word.size()};
+    }
+}
+
+orientation_t Word::get_orientation() const
+{
+    return m_posAndOrient.getOrient();
+}
+
+char Word::at(size_t idx) const
+{
+    if (idx < m_word.size())
+    {
+        return m_word[idx];
+    }
+    throw std::invalid_argument(
+            "Word - at(idx) - given idx is out of bounds\n");
+}
+
+size_t Word::length() const
+{
+    return m_word.size();
+}
+
+RectArea Word::rect_area() const
+{
+    return {pos_t(m_posAndOrient.getPos()),
+            pos_t(this->get_end_position())};
+
+}
+
+// -----RECT AREA CLASS-----
+
+point_placement RectArea::isInside(pos_t p) const
+{
+    if (m_atLeastOneElemExist)
+    {
+        if (p.second < m_topLeft.second)
+        {
+            return OVER;
+        }
+        if (p.second > m_bottomRight.second)
+        {
+            return UNDER;
+        }
+        if (p.first > m_bottomRight.first)
+        {
+            return ON_THE_RIGHT;
+        }
+        if (p.first < m_topLeft.first)
+        {
+            return ON_THE_LEFT;
+        }
+        return INSIDE;
+    }
+    return NO_AREA;
+}
+
+pos_t RectArea::calcArea()
+{
+    m_atLeastOneElemExist = true;
+
+    if (m_bottomRight.first < m_topLeft.first ||
+        m_bottomRight.second < m_topLeft.second)
+    {
+        m_atLeastOneElemExist = false;
+        return {0, 0};
+    }
+
+    return {m_bottomRight.first - m_topLeft.first,
+            m_bottomRight.second - m_topLeft.second};
+}
+
+// Constructors:
+RectArea::RectArea(pos_t topLeft, pos_t bottomRight) : m_topLeft(topLeft),
+                                                       m_bottomRight(
+                                                               bottomRight),
+                                                       m_areaSize(calcArea())
+{
+}
+
+RectArea::RectArea(const RectArea& other) = default;
+//: m_topLeft(other.m_topLeft),
+// m_bottomRight(other.m_bottomRight),
+// m_areaSize(other.m_areaSize),
+// m_atLeastOneElemExist(other.m_atLeastOneElemExist) {}
+
+RectArea::RectArea(RectArea&& other) noexcept = default;
+// : m_topLeft(move(other.m_topLeft)),
+// m_bottomRight(move(other.m_bottomRight)),
+// m_areaSize(move(other.m_areaSize)),
+// m_atLeastOneElemExist(move(other.m_atLeastOneElemExist)) {}
+
+RectArea::~RectArea() = default;
+
+RectArea& RectArea::operator=(const RectArea& rhs)
+{
+    if (this != &rhs)
+    {
+        m_topLeft = rhs.m_topLeft;
+        m_bottomRight = rhs.m_bottomRight;
+        m_areaSize = rhs.m_areaSize;
+        m_atLeastOneElemExist = rhs.m_atLeastOneElemExist;
+    }
     return *this;
+}
+
+RectArea& RectArea::operator=(RectArea&& rhs) noexcept
+{
+    if (this != &rhs)
+    {
+        m_topLeft = rhs.m_topLeft;
+        m_bottomRight = rhs.m_bottomRight;
+        m_areaSize = rhs.m_areaSize;
+        m_atLeastOneElemExist = rhs.m_atLeastOneElemExist;
+        rhs = DEFAULT_EMPTY_RECT_AREA;
+    }
+    return *this;
+}
+
+RectArea& RectArea::operator*=(const RectArea& rhs)
+{
+    if (this->empty())
+    {
+        if (m_atLeastOneElemExist && rhs.isInside(m_topLeft) == INSIDE)
+        {
+            *this = RectArea(m_topLeft, m_topLeft);
+        } else
+        {
+            *this = DEFAULT_EMPTY_RECT_AREA;
+        }
+    } else if (rhs.empty())
+    {
+        if (rhs.m_atLeastOneElemExist &&
+            this->isInside(rhs.m_topLeft) == INSIDE)
+        {
+            *this = RectArea(rhs.m_topLeft, rhs.m_topLeft);
+        } else
+        {
+            *this = DEFAULT_EMPTY_RECT_AREA;
+        }
+    } else
+    {
+        if (m_topLeft.first > rhs.m_bottomRight.first ||
+            m_topLeft.second > rhs.m_bottomRight.second ||
+            m_bottomRight.first < rhs.m_topLeft.first ||
+            m_bottomRight.second < rhs.m_topLeft.second)
+        {
+            *this = DEFAULT_EMPTY_RECT_AREA;
+        } else
+        {
+            pos_t newTopLeft(max(m_topLeft.first, rhs.m_topLeft.first),
+                             max(m_topLeft.second, rhs.m_topLeft.second));
+            pos_t newBottomRight(min(m_topLeft.first, rhs.m_topLeft.first),
+                                 min(m_bottomRight.second,
+                                     rhs.m_bottomRight.second));
+
+            *this = RectArea(newTopLeft, newBottomRight);
+        }
+    }
+
+    return *this;
+}
+
+RectArea RectArea::operator*(const RectArea& rhs) const
+{
+    RectArea newRectArea = *this;
+    newRectArea *= rhs;
+
+    return newRectArea;
+}
+
+
+// Getters:
+pos_t RectArea::get_left_top() const
+{
+    return m_topLeft;
+}
+
+pos_t RectArea::get_right_bottom() const
+{
+    return m_bottomRight;
+}
+
+pos_t RectArea::size() const
+{
+    return m_areaSize;
+}
+
+bool RectArea::empty() const
+{
+    return m_areaSize == pos_t(0, 0);
+}
+
+// Setters:
+void RectArea::set_left_top(pos_t p)
+{
+    m_topLeft = p;
+    m_areaSize = calcArea();
+}
+
+void RectArea::set_right_bottom(pos_t p)
+{
+    m_bottomRight = p;
+    m_areaSize = calcArea();
+}
+
+void RectArea::extend_to_left_or_right(pos_t p)
+{
+    if (p.first < m_topLeft.first)
+    {
+        m_topLeft.first = p.first;
+    } else if (p.first > m_bottomRight.first)
+    {
+        m_bottomRight.first = p.first;
+    }
+}
+
+void RectArea::embrace(pos_t p)
+{
+    // RectArea is empty when has only one point, meaning m_topLeft = m_bottomRight
+    // or when is null set
+    if (!this->empty() || (this->empty() && m_atLeastOneElemExist))
+    {
+        point_placement p_pos = isInside(p);
+
+        switch (p_pos)
+        {
+            case OVER:
+                m_topLeft.second = p.second;
+
+                extend_to_left_or_right(p);
+
+                break;
+            case UNDER:
+                m_bottomRight.second = p.second;
+
+                extend_to_left_or_right(p);
+
+                break;
+            case ON_THE_RIGHT:
+            case ON_THE_LEFT:
+                extend_to_left_or_right(p);
+
+                break;
+            default:
+                break;
+        }
+    } else if (!m_atLeastOneElemExist)
+    {
+        m_topLeft = p;
+        m_bottomRight = p;
+    }
+
+    this->calcArea();
+}
+
+// -----CROSSWORD CLASS-----
+
+Crossword::Crossword(Crossword&& other) noexcept
+        : m_words(std::move(other.m_words)),
+          m_rectArea(std::move(other.m_rectArea)),
+          m_letters(std::move(other.m_letters))
+{
 }
 
 Crossword& Crossword::operator=(Crossword&& other) noexcept
 {
     m_words = std::move(other.m_words);
-    m_rectArea = std::exchange(other.m_rectArea, DEFAULT_EMPTY_RECT_AREA);
+    m_rectArea = std::move(other.m_rectArea);
     m_letters = std::move(other.m_letters);
     return *this;
 }
 
-Crossword::Crossword(const Word &word)
-    : m_rectArea(RectArea(word.get_start_position(), word.get_end_position()))
+Crossword::Crossword(const Word& word)
+        : m_rectArea(RectArea(word.get_start_position(),
+                              word.get_end_position()))
 {
     insert_word(word);
 }
 
 Crossword::Crossword(const Word& firstWord, const std::vector<Word>& words)
-    : m_rectArea(RectArea(firstWord.get_start_position(), firstWord.get_end_position()))
+        : m_rectArea(RectArea(firstWord.get_start_position(),
+                              firstWord.get_end_position()))
 {
     insert_word(firstWord);
-    for(const auto& word : words)
+    for (const auto& word: words)
     {
         insert_word(word);
     }
@@ -44,7 +357,7 @@ Crossword::Crossword(const Word& firstWord, const std::vector<Word>& words)
 
 void Crossword::insert_word(const Word& word)
 {
-    if(!collides(word))
+    if (!collides(word))
     {
         m_words.push_back(word);
         m_rectArea.embrace(word.get_start_position());
@@ -54,16 +367,15 @@ void Crossword::insert_word(const Word& word)
         size_t y = word.getY();
         orientation_t o = word.get_orientation();
 
-        for(int i = 0; i < word.length(); i++)
+        for (int i = 0; i < word.length(); i++)
         {
-            if(o == H)
+            if (o == H)
             {
                 m_letters[pos_t(y, x + i)] = {
                         word.at(i),
                         o,
                         m_letters.count(pos_t(y, x + i)) != 0};
-            }
-            else // (o == V)
+            } else // (o == V)
             {
                 m_letters[pos_t(y + i, x)] = {
                         word.at(i),
@@ -82,89 +394,99 @@ dim_t Crossword::size() const
 dim_t Crossword::word_count() const
 {
     dim_t result = {0, 0};
-    for(const auto& word : m_words)
+    for (const auto& word: m_words)
     {
-        if(word.get_orientation() == H) { result.first++; }
-        else { result.second++; }
+        if (word.get_orientation() == H)
+        { result.first++; }
+        else
+        { result.second++; }
     }
     return result;
 }
 
-// syf
 bool Crossword::collides(const Word& word) const
 {
-    size_t  x = word.getX();
-    size_t  y = word.getY();
+    size_t x = word.getX();
+    size_t y = word.getY();
     orientation_t o = word.get_orientation();
-    Letter letter{};
 
-    if(o == H)
-    {
-        for(int i = 0; i < word.length(); i++)
-        {
-            if(i != word.length() - 1 && x + i == SIZE_MAX) { return true; }
-            if(LETTER_EXISTS(y, x + i))
-            {
-                letter = LETTER(y, x + i);
-                if(letter.character == word.at(i) && letter.orientation != o && !letter.intersection)
-                {
-                    if(y > 0 && LETTER_EXISTS(y - 1, x + i) && LETTER(y - 1, x + i).intersection)
-                    {
-                        return true;
-                    }
-                    if(y < SIZE_MAX && LETTER_EXISTS(y + 1, x + i) && LETTER(y + 1, x + i).intersection)
-                    {
-                        return true;
-                    }
-                }
-                else { return true; }
-            }
-            else // (!(LETTER_EXISTS(y, x + i)))
-            {
-                if(y > 0 && LETTER_EXISTS(y - 1, x + i)) { return true; }
-                if(y < SIZE_MAX && LETTER_EXISTS(y + 1, x + i)) { return true; }
-            }
-        }
-        if(x > 0 && LETTER_EXISTS(y, x - 1)) { return true; }
-        if(x < SIZE_MAX - word.length() + 1 && LETTER_EXISTS(y, x + word.length())) { return true; }
-    }
-    else // (o == V)
+    if (o == H)
     {
         for (int i = 0; i < word.length(); i++)
         {
-            if (i != word.length() - 1 && y + i == SIZE_MAX) { return true; }
-            if (LETTER_EXISTS(y + i, x))
+            if (i != word.length() - 1 && x + i == SIZE_MAX)
+            { return true; }
+            if (LETTER_EXISTS(y, x + i))
             {
-                letter = LETTER(y + i, x);
-                if (letter.character == word.at(i) && letter.orientation != o && !letter.intersection)
+                Letter letter = LETTER(y, x + i);
+                if (letter.character == word.at(i) &&
+                    letter.orientation != o &&
+                    !letter.intersection)
                 {
-                    if (x > 0 && LETTER_EXISTS(y + i, x - 1) && LETTER(y + i, x - 1).intersection)
-                    {
-                        return true;
-                    }
-                    if (x < SIZE_MAX && LETTER_EXISTS(y + i, x + 1) && LETTER(y + i, x + 1).intersection)
-                    {
-                        return true;
-                    }
-                }
-                else { return true; }
-            }
-            else // (!(LETTER_EXISTS(y + i, x)))
+                    if (y > 0 && LETTER_EXISTS(y - 1, x + i) &&
+                        LETTER(y - 1, x + i).intersection)
+                    { return true; }
+                    if (y < SIZE_MAX && LETTER_EXISTS(y + 1, x + i) &&
+                        LETTER(y + 1, x + i).intersection)
+                    { return true; }
+                } else
+                { return true; }
+            } else // (!(LETTER_EXISTS(y, x + i)))
             {
-                if (x > 0 && LETTER_EXISTS(y + i, x - 1)) { return true; }
-                if (x < SIZE_MAX && LETTER_EXISTS(y + i, x + 1)) { return true; }
+                if (y > 0 && LETTER_EXISTS(y - 1, x + i))
+                { return true; }
+                if (y < SIZE_MAX && LETTER_EXISTS(y + 1, x + i))
+                { return true; }
             }
         }
-        if (y > 0 && LETTER_EXISTS(y - 1, x)) { return true; }
-        if (y < SIZE_MAX - word.length() + 1 && LETTER_EXISTS(y + word.length(), x)) { return true; }
+        if (x > 0 && LETTER_EXISTS(y, x - 1))
+        { return true; }
+        if (x < SIZE_MAX - word.length() + 1 &&
+            LETTER_EXISTS(y, x + word.length()))
+        { return true; }
+    } else // (o == V)
+    {
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (i != word.length() - 1 && y + i == SIZE_MAX)
+            { return true; }
+            if (LETTER_EXISTS(y + i, x))
+            {
+                Letter letter = LETTER(y + i, x);
+                if (letter.character == word.at(i) &&
+                    letter.orientation != o &&
+                    !letter.intersection)
+                {
+                    if (x > 0 && LETTER_EXISTS(y + i, x - 1) &&
+                        LETTER(y + i, x - 1).intersection)
+                    { return true; }
+                    if (x < SIZE_MAX && LETTER_EXISTS(y + i, x + 1) &&
+                        LETTER(y + i, x + 1).intersection)
+                    { return true; }
+                } else
+                { return true; }
+            } else // (!(LETTER_EXISTS(y + i, x)))
+            {
+                if (x > 0 && LETTER_EXISTS(y + i, x - 1))
+                { return true; }
+                if (x < SIZE_MAX && LETTER_EXISTS(y + i, x + 1))
+                { return true; }
+            }
+        }
+        if (y > 0 && LETTER_EXISTS(y - 1, x))
+        { return true; }
+        if (y < SIZE_MAX - word.length() + 1 &&
+            LETTER_EXISTS(y + word.length(), x))
+        { return true; }
     }
     return false;
 }
 
 Crossword Crossword::operator+(const Crossword& other) const
 {
-    Crossword crossword(m_words[0], m_words); // The first m_word will collide, so it won't be added.
-    for(const auto& word : other.m_words)
+    Crossword crossword(m_words[0], m_words);
+    // The first m_word will collide, so it won't be added.
+    for (const auto& word: other.m_words)
     {
         crossword.insert_word(word);
     }
@@ -178,11 +500,11 @@ Crossword Crossword::operator+=(const Crossword& other) const
 
 void writeCrosswordLetter(std::ostream& out, char c)
 {
-    if((c >= 'a' && c <= 'z') || (c >= 'A' && c <='Z'))
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
     {
         out << toupper(c);
-    }
-    else { out << DEFAULT_CHAR; }
+    } else
+    { out << DEFAULT_CHAR; }
 }
 
 std::ostream& operator<<(std::ostream& out, const Crossword& crossword)
@@ -195,19 +517,26 @@ std::ostream& operator<<(std::ostream& out, const Crossword& crossword)
     end.second++;
     auto pointsItr = crossword.m_letters.cbegin();
 
-    for(size_t j = start.second; j <= end.second; j++)
+    for (size_t j = start.second; j <= end.second; j++)
     {
-        for(size_t i = start.first; i < end.first; i++)
+        for (size_t i = start.first; i < end.first; i++)
         {
-            if(pointsItr != crossword.m_letters.cend() && (*pointsItr).first == pos_t(j, i))
+            if (pointsItr != crossword.m_letters.cend() &&
+                (*pointsItr).first == pos_t(j, i))
             {
                 writeCrosswordLetter(out, (*pointsItr).second.character);
                 pointsItr++;
-            }
-            else { out << CROSSWORD_BACKGROUND; }
+            } else
+            { out << CROSSWORD_BACKGROUND; }
             out << ' ';
         }
         out << CROSSWORD_BACKGROUND;
         out << std::endl;
     }
+    return out;
+}
+
+int main()
+{
+    return 0;
 }
--- crosswords.h
+++ crosswords.h
@@ -4,6 +4,7 @@
 #include <map>
 #include <vector>
 #include <iostream>
+#include <compare>
 
 using pos_t = std::pair<size_t, size_t>;
 using dim_t = std::pair<size_t, size_t>;
@@ -16,6 +17,11 @@ enum orientation_t : bool
 
 namespace
 {
+    enum point_placement
+    {
+        ON_THE_RIGHT, ON_THE_LEFT, OVER, UNDER, INSIDE, NO_AREA
+    };
+
     class WordPos
     {
     private:
@@ -24,23 +30,42 @@ namespace
 
     public:
         // Constructors:
-        inline WordPos() = delete;
+        WordPos() = delete;
+
         inline WordPos(size_t x, size_t y, orientation_t _orient)
-            : m_pos(x, y), m_orient(_orient)
+                : m_pos(x, y), m_orient(_orient)
         {
         }
-        inline WordPos(pos_t p, orientation_t o)
-            : m_pos(p), m_orient(o)
+
+        inline WordPos(pos_t const& p, orientation_t const& o)
+                : m_pos(p), m_orient(o)
         {
         }
+
+        // Copy constructor:
+        WordPos(const WordPos& w) = default;
+        //: m_pos(w.m_pos), m_orient(w.m_orient) {}
+
+        // Move Constructor:
+        WordPos(WordPos&& w) = default;
+        //: m_pos(move(w.m_pos)), m_orient(move(w.m_orient)) {}
 
         // Destructors:
-        inline ~WordPos() = default;
+        ~WordPos() = default;
+
+        // Operators:
+        WordPos& operator=(const WordPos& rhs) = default;
+
+        WordPos& operator=(WordPos&& rhs) = default;
+
+        std::strong_ordering operator<=>(const WordPos&) const = default;
 
         // Getters:
-        inline pos_t getPos() const { return m_pos; }
-        inline orientation_t getOrient() const { return m_orient; }
+        inline pos_t getPos() const
+        { return m_pos; }
 
+        inline orientation_t getOrient() const
+        { return m_orient; }
     };
 
     struct Letter
@@ -51,6 +76,55 @@ namespace
     };
 }
 
+class RectArea
+{
+private:
+    pos_t m_topLeft, m_bottomRight;
+    pos_t m_areaSize;
+    bool m_atLeastOneElemExist;
+
+    pos_t calcArea();
+
+    point_placement isInside(pos_t) const;
+
+    void extend_to_left_or_right(pos_t);
+
+public:
+    RectArea() = delete;
+
+    RectArea(pos_t _topLeft, pos_t _bottomRight);
+
+    RectArea(const RectArea&);
+
+    RectArea(RectArea&&) noexcept;
+
+    ~RectArea();
+
+    RectArea& operator=(const RectArea&);
+
+    RectArea& operator=(RectArea&&) noexcept;
+
+    RectArea& operator*=(const RectArea& rhs);
+
+    RectArea operator*(const RectArea& rhs) const;
+
+    // Getters:
+    pos_t get_left_top() const;
+
+    pos_t get_right_bottom() const;
+
+    pos_t size() const;
+
+    bool empty() const;
+
+    // Setters:
+    void set_left_top(pos_t);
+
+    void set_right_bottom(pos_t);
+
+    void embrace(pos_t);
+};
+
 class Word
 {
 private:
@@ -60,54 +134,68 @@ private:
 public:
     // Constructors:
     Word() = delete;
-    Word(size_t x, size_t y, orientation_t orient, const std::string& word);
+
+    Word(size_t x, size_t y, orientation_t orient, std::string const& _word);
+
+    // Copy constructor:
+    Word(const Word& other);
+    //: m_posAndOrient(other.m_posAndOrient),
+    //    m_word(other.m_word) {}
+
+    // Move constructor:
+    Word(Word&& other) noexcept;
+    //: m_posAndOrient(move(other.m_posAndOrient)),
+    //    m_word(move(other.m_word)) {}
 
     // Destructors:
-    inline ~Word() = default;
+    ~Word();
+
+    // Operators:
+    Word& operator=(const Word& rhs);
+
+    Word& operator=(Word&& rhs) noexcept;
+
+    bool operator==(const Word& other) const;
+
+    auto operator<=>(const Word& other) const;
 
     // Getters:
     pos_t get_start_position() const;
+
     pos_t get_end_position() const;
+
     orientation_t get_orientation() const;
+
     char at(size_t idx) const;
+
     size_t length() const;
-    size_t getX() const;
-    size_t getY() const;
 
-};
+    RectArea rect_area() const;
 
-class RectArea
-{
-private:
-    pos_t m_leftTop;
-    pos_t m_rightBottom;
+    inline size_t getX() const
+    { return m_posAndOrient.getPos().first; }
 
-public:
-    // Constructors:
-    RectArea() = delete;
-    RectArea(pos_t leftTop, pos_t rightBottom);
-
-    // Destructors:
-    inline ~RectArea() = default;
-
-    // Getters:
-    pos_t get_left_top() const;
-    pos_t get_right_bottom() const;
+    inline size_t getY() const
+    { return m_posAndOrient.getPos().second; }
 };
 
 class Crossword
 {
 private:
-    std::vector<Word> m_words; // can be deleted, but a workaround for word_count() will have to be implemented
+    std::vector<Word> m_words;
     RectArea m_rectArea;
     std::map<pos_t, Letter> m_letters;
 
 public:
     // Constructors:
     Crossword() = delete;
+
     Crossword(const Crossword& other) = default;
+
     Crossword(Crossword&& other) noexcept;
+
     explicit Crossword(const Word& word);
+
     Crossword(const Word& firstWord, const std::vector<Word>& words);
 
     // Destructors:
@@ -117,23 +205,29 @@ public:
 
     // Getters:
     dim_t size() const;
+
     dim_t word_count() const;
 
     // Operators:
-    Crossword& operator=(const Crossword& other);
+    Crossword& operator=(const Crossword& other) = default;
+
     Crossword& operator=(Crossword&& other) noexcept;
+
     Crossword operator+(const Crossword& other) const;
+
     Crossword operator+=(const Crossword& other) const;
-    friend std::ostream& operator<<(std::ostream& out, const Crossword& crossword);
+
+    friend std::ostream&
+    operator<<(std::ostream& out, const Crossword& crossword);
 
 private:
     bool collides(const Word& word) const;
 
 };
 
-const RectArea DEFAULT_EMPTY_RECT_AREA(pos_t(1, 1), pos_t(0, 0));
-char DEFAULT_CHAR = '?';
-char CROSSWORD_BACKGROUND = '.';
-std::string DEFAULT_WORD = "?";
+inline const RectArea DEFAULT_EMPTY_RECT_AREA(pos_t(1, 1), pos_t(0, 0));
+inline char DEFAULT_CHAR = '?';
+inline char CROSSWORD_BACKGROUND = '.';
+inline std::string DEFAULT_WORD = "?";
 
 #endif //III_PROJEKTCPP_KRZYZOWKA_CROSSWORD_H
